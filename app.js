ba22f04d0f7b67493db9d8643c078d7650e0a094
KrishnaBaheti <67362840+KrishnaBaheti@users.noreply.github.com>
2021-06-05 23:12:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+var app = Express();
+// const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url, { useNewUrlParser : true, useUnifiedTopology: true }, { useUnifiedTopology: true });
+const dbName = 'agrigrowth';
+
+var myobj = new Object;
+var db;
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(url, function(err, db) {
+if (err) throw err;
+var dbo = db.db("agrigrowth");
+  var myobj1 = { soilType: null, temperature: null, humidity: null, ph: null, rainfall: null, crop: null };
+  console.log(myobj1);
+
+  dbo.collection("SoilTestingInfo").insertOne(myobj1, function(error, res) {
+          if(error) {
+              console.error(error);
+          }
+  });
+
+  var myobj2 = { state: null, district: null, cropYear: null, season: null, crop: null, area: null, production: null };
+  console.log(myobj2);
+
+  dbo.collection("CropInfo").insertOne(myobj2, function(error, res) {
+          if(error) {
+              console.error(error);
+          }
+  });
+
+  var myobj3 = { email: null, password: null };
+  console.log(myobj3);
+
+  dbo.collection("LoginInfo").insertOne(myobj3, function(error, res) {
+          if(error) {
+              console.error(error);
+          }
+  });
+
+  var myobj4 = { name: null, email: null, password: null, phoneNo: null, address: null, landmark: null, pincode: null, district: null, state: null, sizeOfLand: null, waterSource: null, infoAboutCrop: null };
+  console.log(myobj4);
+
+  dbo.collection("UserInfo").insertOne(myobj4, function(error, res) {
+          if(error) {
+              console.error(error);
+          }
+  });
+
+  app.get("/userProfile", function(req, res) {
+            console.log("User Profile Form");
+      });
+
+  app.post("/login", function(req, res) {
+            console.log("Login");
+      });
+
+  app.post("/signUp", function(req, res) {
+            console.log("SignUp");
+      });
+
+  app.post("/signUp2", function(req, res) {
+            console.log("SignUp 2");
+      });
+
+  app.post("/soilTestingForm", function(req, res) {
+            console.log("Soil Testing Form");
+      });
+
+  db.close();
+});
+
+app.listen(3000, function() {
+  console.log("Server started. ");
+});
