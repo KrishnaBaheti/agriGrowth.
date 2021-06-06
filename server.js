e7be476bb3b0a104c65d52df347fd00b69debca0
KrishnaBaheti <67362840+KrishnaBaheti@users.noreply.github.com>
2021-06-06 22:44:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ app.post("/signUp", (req, res) => {
     var myData = new User(req.body);
     myData.save()
         .then(item => {
-            res.send("Name saved to database");
+            console.log("Name saved to database");
         })
         .catch(err => {
-            res.status(400).send("Unable to save to database");
+            console.log("Unable to save to database");
         });
 });
 
